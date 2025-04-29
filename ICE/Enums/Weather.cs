f099b052e953a3055244cf383196cdf7523dddc8
pot0to <19513249+pot0to@users.noreply.github.com>
2025-04-29 00:50:04 -0700

    added some support for weather and time, will fix it later

---
+++ ICE/Enums/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE.Enums
+{
+    public enum CosmicWeather
+    {
+        FairSkies,
+
+        UmbralWind, // WKSMissionUnit.Unknown7 13
+        MoonDust    // WKSMissionUnit.Unknown7 14
+    }
+}
