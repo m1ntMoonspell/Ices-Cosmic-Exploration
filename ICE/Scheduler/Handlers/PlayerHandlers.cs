634ec245cc16784a41d8f031f6d9c83fb3a3fe5e
SubaruYashiro <subaruyashiro@gmail.com>
2025-05-03 07:27:22 +0800

    Add Enable Auto Sprint

--- ICE/Scheduler/Handlers/PlayerHandlers.cs
+++ ICE/Scheduler/Handlers/PlayerHandlers.cs
@@ -1,3 +1,5 @@
+using Dalamud.Game.ClientState.Conditions;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using System.Collections.Generic;
@@ -18,6 +20,7 @@ internal static unsafe class PlayerHandlers
             { (16, 17), new[] { "LTW" } },
             { (20, 21), new[] { "WVR" } }
         };
+    private static readonly uint stellarSprintID = 4398;
 
     public static float Distance(this Vector3 v, Vector3 v2)
     {
@@ -32,6 +35,19 @@ internal static unsafe class PlayerHandlers
     {
         if (IsInZone(1237) && UsingSupportedJob() && C.ShowOverlay) ICE.P.overlayWindow.IsOpen = true;
         else ICE.P.overlayWindow.IsOpen = false;
+
+        if (C.EnableAutoSprint && IsInZone(1237) && !HasStatusId(stellarSprintID) && Svc.Condition[ConditionFlag.NormalConditions] && IsMoving())
+        {
+            UseSprint();
+        } 
+    }
+
+    private static void UseSprint()
+    {
+        var am = ActionManager.Instance();
+        var isSprintReady = am->GetActionStatus(ActionType.GeneralAction, 4) == 0;
+
+        if (isSprintReady) am->UseAction(ActionType.GeneralAction, 4);
     }
 
     private static (long, long) GetEorzeaTime()
