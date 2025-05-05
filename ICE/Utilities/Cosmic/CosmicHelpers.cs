19601869add85cf9ace54839ae9203346dbd2590
SubaruYashiro <subaruyashiro@gmail.com>
2025-05-06 04:46:38 +0800

    New Stage 9 Checks
    - New Critical (Astro Magnetic) Variant
    - Added Checks for stage development for timed mission bonuses
    - Put the ExcelSheet Assignment into Init()
    - Refactor variable name TaskMissionFind, also add each enabled mission to query
    - Added Logic to check if there is timed mission enabled at this time

--- ICE/Utilities/Cosmic/CosmicHelpers.cs
+++ ICE/Utilities/Cosmic/CosmicHelpers.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.Generic;
 using FFXIVClientStructs.FFXIV.Client.Game.WKS;
+using Lumina.Excel;
+using Lumina.Excel.Sheets;
 using static ECommons.UIHelpers.AddonMasterImplementations.AddonMaster;
 
 namespace ICE.Utilities;
 
 public static partial class CosmicHelper
 {
+    private static ExcelSheet<WKSDevGrade>? DevGrade;
+
     public static MissionListInfo CurrentMissionInfo => MissionInfoDict[CurrentLunarMission];
     public static MoonRecipieInfo CurrentMoonRecipe => MoonRecipies[CurrentLunarMission];
     public static unsafe uint CurrentLunarMission => WKSManager.Instance()->CurrentMissionUnitRowId;
+    public static unsafe uint CurrentLunarDevelopment => DevGrade.GetRow(WKSManager.Instance()->DevGrade).Unknown6;
     public static Dictionary<int, string> ExpDictionary = new()
     {
         { 1, "I" },
@@ -16,6 +21,11 @@ public static partial class CosmicHelper
         { 3, "III" },
         { 4, "IV" }
     };
+
+    public static void Init()
+    {
+        DevGrade ??= Svc.Data.GetExcelSheet<WKSDevGrade>();
+    }
 
     public static void OpenStellaMission()
     {
